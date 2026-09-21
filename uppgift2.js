27c9e8a55b06c355087a66530f88e96c50cf1ba1
miem85 <emanuelsson.mikael85@gmail.com>
2026-09-21 18:12:56 +0200

    Skrev variabler för uppgift 2

---
+++ uppgift2.js
@@ -0,0 +1,9 @@
+/* Uppgift 2 - Operatorer och beräkningar. Av Mikael Emanuelsson, 2026 */
+
+"use strict";
+
+const price = 149;
+const quantity = 4;
+
+const totalPrice = price * quantity;
+const totalWithVat = totalPrice * 1.25;
