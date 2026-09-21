bd2f7d858a765f81c754afb24d0ca4bd298e9919
miem85 <emanuelsson.mikael85@gmail.com>
2026-09-21 18:07:50 +0200

    Skapade variabler och utskrift uppgift 1

---
+++ uppgift1.js
@@ -0,0 +1,11 @@
+/* Uppgift 1 - Variabler och datatyper. Av Mikael Emanuelsson, 2026 */
+"use strict";
+
+const firstName = "Mikael";
+const lastName = "Emanuelsson";
+const age = 40;
+const isStudent = true;
+
+console.log("Namn: " + firstName + " " + lastName);
+console.log("Ålder: " + age + " år");
+console.log("Student: " + isStudent);
