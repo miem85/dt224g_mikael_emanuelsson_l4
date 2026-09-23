7b76a311beff9fabbcd87379df383e4d4e294e8b
miem85 <emanuelsson.mikael85@gmail.com>
2026-09-23 15:44:37 +0200

    Skapade en funktion för areaberäkning upp 6

---
+++ uppgift6.js
@@ -0,0 +1,12 @@
+/* Uppgift 6 - Funktioner. Av Mikael Emanuelsson, 2026 */
+
+"use strict";
+
+function calculateArea(width, height) {
+    const area = width * height;
+    return area;
+}
+
+console.log("Arean är " + calculateArea(3, 7) + " cm²");
+console.log("Arean är " + calculateArea(7, 14) + " cm²");
+console.log("Arean är " + calculateArea(55, 3) + " cm²");
